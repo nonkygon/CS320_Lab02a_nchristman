0b4fe0d644f4bb2f5323a1482d2b3a7442735f50
nonkygon <nchristman@ycp.edu>
2022-02-11 15:51:55 -0500

    The beginning (creating the git project)

---
+++ CS320_Lab02a_nchristman/junit/edu/ycp/cs320/lab02/model/GuessingGameTest.java
@@ -0,0 +1,56 @@
+package edu.ycp.cs320.lab02.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02.model.GuessingGame;
+
+public class GuessingGameTest {
+	private GuessingGame model;
+	
+	@Before
+	public void setUp() {
+		model = new GuessingGame();
+		model.setMin(1);
+		model.setMax(100);
+	}
+	
+	@Test
+	public void testSetMin() {
+		assertEquals(1, model.getMin());
+	}
+	
+	@Test
+	public void testSetMax() {
+		assertEquals(100, model.getMax());
+	}
+	
+	@Test
+	public void testIsDone() {
+		assertFalse(model.isDone());
+		model.setMin(50);
+		model.setMax(50);
+		assertTrue(model.isDone());
+	}
+	
+	@Test
+	public void testGetGuess() {
+		assertEquals(50, model.getGuess());
+	}
+	
+	@Test
+	public void testIsLessThan() {
+		int maximum = 50;
+		model.setIsLessThan(maximum);
+		assertEquals(maximum-1, model.getMax());
+	}
+	
+	@Test
+	public void testIsGreaterThan() {
+		int minimum = 50;
+		model.setIsGreaterThan(minimum);
+		assertEquals(minimum+1, model.getMin());
+	}
+}
