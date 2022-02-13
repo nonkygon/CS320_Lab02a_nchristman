b4f766034354250aa2057f706cbda1792b30634a
nonkygon <nchristman@ycp.edu>
2022-02-13 14:44:14 -0500

    Add Numbers Upgrade
    Added a third parameter to the AddNumbers program

--- CS320_Lab02a_nchristman/src/edu/ycp/cs320/lab02a_nchristman/controller/AddNumbersController.java
+++ CS320_Lab02a_nchristman/src/edu/ycp/cs320/lab02a_nchristman/controller/AddNumbersController.java
@@ -1,7 +1,7 @@
 package edu.ycp.cs320.lab02a_nchristman.controller;
 
 public class AddNumbersController {
-	public Double add(Double first, Double second) {
-		return first + second;
+	public Double add(Double first, Double second, Double third) {
+		return first + second + third;
 	}
 }
