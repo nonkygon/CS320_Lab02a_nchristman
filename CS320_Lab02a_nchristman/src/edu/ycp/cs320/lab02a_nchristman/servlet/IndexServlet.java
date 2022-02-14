34266837cd39487cbac59aba68f647e4d64d6f7a
nonkygon <nchristman@ycp.edu>
2022-02-14 16:10:25 -0500

    Index Page update
    -added actual content to the index page
    -also made it look cool

--- CS320_Lab02a_nchristman/src/edu/ycp/cs320/lab02a_nchristman/servlet/IndexServlet.java
+++ CS320_Lab02a_nchristman/src/edu/ycp/cs320/lab02a_nchristman/servlet/IndexServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ycp.cs320.lab02a_nchristman.controller.NumbersController;
+
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -17,5 +19,29 @@ public class IndexServlet extends HttpServlet {
 		System.out.println("Index Servlet: doGet");
 		
 		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Index Servlet: doPost");
+		
+		String submit = req.getParameter("submit");
+		System.out.println(submit);
+		
+		if(submit.equals("Add Numbers!")) {
+			req.getRequestDispatcher("/_view/addNumbers.jsp").forward(req, resp);
+		}
+		else if(submit.equals("Multiply Numbers!")) {
+			req.getRequestDispatcher("/_view/multiplyNumbers.jsp").forward(req, resp);
+		}
+		else if(submit.equals("Guessing Game!")) {
+			req.getRequestDispatcher("/_view/guessingGame.jsp").forward(req, resp);
+		}
+		else {
+			System.out.println("Invalid Link");
+		}
+		
 	}
 }
--- CS320_Lab02a_nchristman/war/_view/index.jsp
+++ CS320_Lab02a_nchristman/war/_view/index.jsp
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 
 <html>
+	<style>
+		.button{
+			background-color: #E92900;
+			border-color: #FFFFFF;
+			color: #FFFFFF;
+			font-size: 48px;
+			height: 100px;
+			width: 100%;
+			float: left;
+			transition: 0.5s;
+		}
+		.button:hover{
+			color: #E92900;
+			border-color: #E92900;
+			background-color: #FFFFFF;
+		}
+	</style>
 	<head>
 		<title>Index view</title>
 	</head>
 
-	<body>
-		This is the index view jsp
+	<body style = "background-color: #000000;">
+		<form action="${pageContext.servletContext.contextPath}/index" method="post">
+			<div>
+				<input class="button" type="Submit" name="submit" value="Add Numbers!"/>
+			</div>
+			<div>
+				<input class="button" type="Submit" name="submit" value="Multiply Numbers!"/>
+			</div>
+			<div>
+				<input class="button" type="Submit" name="submit" value="Guessing Game!"/>
+			</div>
+		</form>
 	</body>
 </html>
